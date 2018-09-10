d993b0085058e0017d023d520b06ced13442c155
zonbimutin <38836980+zonbimutin@users.noreply.github.com>
2018-09-11 00:30:43 +0200

    JBBlog

--- JBBlog/Controller/ViewController.swift
+++ JBBlog/Controller/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
     }
 
-
+    @IBAction func factiryButton(_ sender: UIButton) {
+        print("boton")
+        
+        
+    }
+    
 }
 
