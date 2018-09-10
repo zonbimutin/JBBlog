3bfc557f54adb9c76be07037ff6ed0dfd2f815f6
zonbimutin <38836980+zonbimutin@users.noreply.github.com>
2018-09-10 13:39:59 +0200

    Initial Commit

---
+++ JBBlog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JBBlog
+//
+//  Created by Felipe Alarcon on 10/09/2018.
+//  Copyright © 2018 Felipe Alarcon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
